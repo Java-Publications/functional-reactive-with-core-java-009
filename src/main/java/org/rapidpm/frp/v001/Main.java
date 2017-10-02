c12d44c27009c1626d0fc448a85bcd9d95ca5e4f
Sven Ruppert <sven.ruppert@gmail.com>
2017-10-02 17:17:54 +0200

    v001

---
+++ src/main/java/org/rapidpm/frp/v001/Main.java
@@ -0,0 +1,34 @@
+package org.rapidpm.frp.v001;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+/**
+ *
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    final CompletableFuture<Void> cfRunnableA = CompletableFuture.runAsync(new Runnable() {
+      @Override
+      public void run() {
+        System.out.println("Hello reactive World");
+      }
+    });
+
+    final CompletableFuture<Void> cfRunnableB = CompletableFuture
+        .runAsync(() -> System.out.println("Hello reactive World"));
+
+
+    final CompletableFuture<String> cfCallableA = CompletableFuture.supplyAsync(new Supplier<String>() {
+      @Override
+      public String get() {
+        return "Hello reactive World";
+      }
+    });
+
+    final CompletableFuture<String> cfCallableB = CompletableFuture
+        .supplyAsync(() -> "Hello reactive World");
+
+  }
+}
